ad234d22929b44cad73d21f8ef3882ff1bd15867
Dave <d.r.young@qub.ac.uk>
2023-04-04 13:05:44 +0100

    x

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ install_requires = [
 # READ THE DOCS SERVERS
 exists = os.path.exists("/home/docs/")
 if exists:
+    install_requires = []
     c_exclude_list = ['healpy', 'astropy',
                       'numpy', 'sherlock', 'wcsaxes', 'HMpTy', 'ligo-gracedb', 'ligo.skymap', 'astropy==5.1.0', 'gcn-kafka', 'astropy_healpix']
     for e in c_exclude_list:
