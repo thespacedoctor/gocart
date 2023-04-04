a5e8ca461b2ac0f224f87552453d66deabc5ff37
Dave <d.r.young@qub.ac.uk>
2023-04-04 12:38:48 +0100

    x

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
 
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.colors',
-                'matplotlib.pyplot', 'matplotlib.cm', 'matplotlib.path', 'matplotlib.patches', 'matplotlib.projections', 'matplotlib.projections.geo', 'healpy', 'astropy', 'astropy.io', 'pylibmc', 'HMpTy', 'HMpTy.mysql', 'ligo', 'ligo.gracedb', 'ligo.gracedb.rest', 'pandas', 'astropy-healpix']
+                'matplotlib.pyplot', 'matplotlib.cm', 'matplotlib.path', 'matplotlib.patches', 'matplotlib.projections', 'matplotlib.projections.geo', 'healpy', 'astropy', 'astropy.io', 'pylibmc', 'HMpTy', 'HMpTy.mysql', 'ligo.skymap', 'ligo', 'ligo.gracedb', 'ligo.gracedb.rest', 'pandas', 'astropy-healpix']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
